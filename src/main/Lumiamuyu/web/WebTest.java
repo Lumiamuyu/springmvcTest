61f1a4b7cb0c58febb711f430453f825be5cb445
Lumiamuyu <lumiamuyu@live.com>
2019-01-04 16:14:44 +0800

    2019/1/4

--- src/main/Lumiamuyu/web/WebTest.java
+++ src/main/Lumiamuyu/web/WebTest.java
@@ -2,6 +2,6 @@ package Lumiamuyu.web;
 
 public class WebTest {
     public static void main(String[] args) {
-        System.out.println("Test OK");
+        System.out.println("Test OK!");
     }
 }
